85f4eacc56c9b2d67442405a3de54545b0ca0417
Ruslan <ruslanmehdiyev922@gmail.com>
2022-12-20 21:37:03 +0400

    updated search

---
+++ PracticeTasks/filltablebycount/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState("");
+  const [newData, setNewData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://northwind.vercel.app/api/orders")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+      console.log("Fetch Rendered");
+  }, []);
+
+  const handleClick = () => {
+    setNewData(data.slice(0, count));
+  };
+
+  return (
+    <div className="App">
+      <label htmlFor="fetch">Count</label>
+      <input
+        type="text"
+        name="fetch"
+        onChange={(e) => setCount(e.target.value)}
+      />
+      <button onClick={handleClick}>GET</button>
+      <ul>
+        {newData && newData.map((e) => <li key={e.id}>{e.customerId}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- PracticeTasks/filltablefetch/src/App.jsx
+++ PracticeTasks/filltablefetch/src/App.jsx
@@ -3,23 +3,12 @@ import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
   const [searchData, setSearchData] = useState([]);
+
   useEffect(() => {
     fetch("https://northwind.vercel.app/api/orders")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
-
-  useEffect(() => {
-    console.log(searchData);
-    const filteredData = data.filter((e) => {
-      if (e.customerId.toLowerCase().includes(searchData)) {
-        return e;
-      } else if (searchData === "") {
-        setData(e);
-      }
-    });
-    setData(filteredData);
-  }, [searchData]);
 
   return (
     <div className="App">
@@ -42,17 +31,23 @@ function App() {
         </thead>
         <tbody>
           {data &&
-            data.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.customerId}</td>
-                <td>{item.employeeId}</td>
-                <td>{item.orderDate.substring(0, 4)}</td>
-                <td>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            ))}
+            data
+              .filter((e) => {
+                return searchData == ""
+                  ? e
+                  : e.customerId.toLowerCase().includes(searchData);
+              })
+              .map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.customerId}</td>
+                  <td>{item.employeeId}</td>
+                  <td>{item.orderDate.substring(0, 4)}</td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
